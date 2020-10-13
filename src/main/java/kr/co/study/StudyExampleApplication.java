d1c285e77ace74d36828a2ae272a44a18861da8e
take <take004@take0801.onmicrosoft.com>
2020-10-13 17:02:56 +0900

    create study project

---
+++ src/main/java/kr/co/study/StudyExampleApplication.java
@@ -0,0 +1,13 @@
+package kr.co.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyExampleApplication.class, args);
+	}
+
+}
